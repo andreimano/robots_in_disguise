c8b5db268d1c8dd944e9dc2a6ad603e5d137a13e
Andrei Manolache <andrei_mano@outlook.com>
2020-05-14 12:27:08 +0000

    removed .vscode
    Signed-off-by: Andrei Manolache <andrei_mano@outlook.com>

--- sketches/transformer_lm.py
+++ sketches/transformer_lm.py
@@ -9,7 +9,6 @@ from models.TransformerLM import TransformerLM
 from utils.data import *
 from utils.train import *
 
-#%%
 batch_size = 32
 eval_batch_size = 32
 
@@ -19,7 +18,6 @@ train_data = batchify(train_txt, batch_size, TEXT)
 val_data = batchify(val_txt, eval_batch_size, TEXT)
 test_data = batchify(test_txt, eval_batch_size, TEXT)
 
-# %%
 bptt = 35
 n_tokens = len(TEXT.vocab.stoi)
 emb_sz = 200
@@ -28,11 +26,20 @@ n_layers = 2
 n_attn_heads = 2
 dropout = 0.2
 
-#%%
 model = TransformerLM(n_tokens, emb_sz, n_attn_heads, n_hid, n_layers, dropout)
 model = model.to(device)
 
 crit = nn.CrossEntropyLoss()
 lr = 5.0
 optim = torch.optim.SGD(model.parameters(), lr=lr)
-scheduler = torch.optim.lr_scheduler.StepLR(optim, 1.0, gamma=0.95)
+sched = torch.optim.lr_scheduler.StepLR(optim, 1.0, gamma=0.95)
+
+#%%
+best_val_loss = float("inf")
+epochs = 10
+best_model = None
+
+for epoch in range(1, epochs + 1):
+    train(model, crit, optim, lr, sched, epoch, train_data, TEXT.vocab, bptt)
+    sched.step()
+# %%
